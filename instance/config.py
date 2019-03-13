c576d619d61ff39a8fdfa36c182beb82d27643ab
jmwania <josphatmwania237@gmail.com>
2019-03-13 10:58:11 +0300

    [20191303- Bucketlist]

---
+++ instance/config.py
@@ -0,0 +1,61 @@
+import os
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+
+class DevelopmentConfig(Config):
+    """configurations for developmet.""""
+    DEBUG = True
+
+class TestingConfig(Config):
+    """Configurations for testing, with a separate test database."""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    DEBUG = True
+
+class StaggingConfig(Config):
+    """Configurations for stagging"""
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StaggingConfig,
+    'production': ProductionConfig,
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
